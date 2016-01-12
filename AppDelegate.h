d6d371cb64fa944778c78e23be5fe0f8b21c0af2
Esha Massand <ubemas001@mail.bbk.ac.uk>
2016-01-12 14:14:18 +0000

    delegate

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoffeeApp
+//
+//  Created by Esha Massand on 28/12/2015.
+//  Copyright © 2015 Esha Massand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
