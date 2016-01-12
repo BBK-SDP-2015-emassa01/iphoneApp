bc654156b55988b4b7fda3ec75fee95de15a7e88
Esha Massand <ubemas001@mail.bbk.ac.uk>
2016-01-12 14:16:27 +0000

    all in one

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoffeeApp
+//
+//  Created by Esha Massand on 28/12/2015.
+//  Copyright © 2015 Esha Massand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoffeeApp
+//
+//  Created by Esha Massand on 28/12/2015.
+//  Copyright © 2015 Esha Massand. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
